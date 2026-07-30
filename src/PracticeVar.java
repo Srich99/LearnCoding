2cf47abf76728d9f16ae49c20c12aca417bdb462
Sri <chebrolusrilakshmi39@gmail.com>
2026-07-30 07:21:44 -0400

    Day 2 Variables and data types practice is done

---
+++ src/PracticeVar.java
@@ -0,0 +1,27 @@
+public class PracticeVar {
+    public static void main(String[] args) {
+       String name= "Srilakshmi";
+       int age= 25;
+       String city="Baltimore";
+
+       double lapPrice= 579.99;
+       double disRate= 20.2;
+
+       char letter= 'S';
+       boolean likeIt= true;
+
+       byte a=2;
+       short b= 1000;
+       int c=2222222;
+       long d=1234567890L;
+       float e=12345.67f;
+       double f=1213326578.345;
+       char g='e';
+       boolean h=false;
+        System.out.println("My name: "+name);
+        System.out.println("My age:"+age);
+        System.out.println(city);
+
+
+    }
+}
